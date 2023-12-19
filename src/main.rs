963c99842c5d511c232540dd9654b85b66ef84f8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-18 19:19:36 -0800

    I don't know how to Rust (yet)
    I want to constrain the type for `ListTag`, but I don't know how to set that to an enum, it doesn't seem to like that.
    
    Thanks to LPT4 for inspiration in learning how all this works, little step by little.
    
    println!("Not sure what the use of {:?} is specifically for.", "```rust
    #[derive(Clone, PartialEq, Debug)]
    ```");
    
    "What use is a file format if you don't have anything to represent with it?"
    "Lossless poop emoji"
    
    https://github.com/Minecraft-Manipulator/mm_io
    https://www.google.com/search?q=rust+enum+vs+type
    https://stackoverflow.com/questions/20922091/how-do-you-use-parent-module-imports-in-rust
    https://stackoverflow.com/questions/45519176/how-do-i-use-or-import-a-local-rust-file
    https://stackoverflow.com/questions/70912085/importing-in-rust
    https://stackoverflow.com/questions/36590549/what-is-the-syntax-to-match-on-a-reference-to-an-enum
    https://stackoverflow.com/questions/41648339/how-to-specify-the-underlying-type-of-an-enum-in-rust
    https://doc.rust-lang.org/rust-by-example/generics/bounds.html (generic 'extends' constraint, I think?)
    https://stackoverflow.com/questions/69078910/trait-bounds-did-not-work-out-for-enums-as-generic-parameter
    https://stackoverflow.com/questions/35568871/is-it-possible-to-implement-methods-on-type-aliases
    https://doc.rust-lang.org/stable/book/ch19-04-advanced-types.html#using-the-newtype-pattern-for-type-safety-and-abstraction
    https://stackoverflow.com/questions/51567350/can-traits-be-used-on-enum-types
    https://users.rust-lang.org/t/binding-struct-generic-type-parameter-by-enum/84639/10
    https://stackoverflow.com/questions/69078910/trait-bounds-did-not-work-out-for-enums-as-generic-parameter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::fs;
+mod tag;
+// use crate::tag::{ Tag, IntTag };
+use crate::tag::Tag;
 
 fn main() {
     let file: &str = "./test/hello_world.nbt";
@@ -6,4 +9,10 @@ fn main() {
 
     let data: Vec<u8> = fs::read(file).unwrap();
     println!("{:?}",&data);
+
+    let haha: Tag = Tag::Byte(25);
+    println!("{:?}",haha);
+
+    // let haha2: IntTag = Tag::Int(25);
+    // println!("{:?}",haha2);
 }
---
+++ src/tag.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+#[repr(u8)]
+#[derive(Debug)]
+pub enum Tag {
+  Byte(ByteTag) = 1,
+  Short(ShortTag),
+  Int(IntTag),
+  Long(LongTag),
+  Float(FloatTag),
+  Double(DoubleTag),
+  ByteArray(ByteArrayTag),
+  String(StringTag),
+  List(ListTag<Tag>),
+  Compound(CompoundTag),
+  IntArray(IntArrayTag),
+  LongArray(LongArrayTag)
+}
+
+pub type ByteTag = i8;
+pub type ShortTag = i16;
+pub type IntTag = i32;
+pub type LongTag = i64;
+pub type FloatTag = f32;
+pub type DoubleTag = f64;
+pub type ByteArrayTag = Vec<i8>;
+pub type StringTag = String;
+pub type ListTag<T/*: Tag*/> = Vec<T>;
+pub type CompoundTag = HashMap<String, Tag>;
+pub type IntArrayTag = Vec<i32>;
+pub type LongArrayTag = Vec<i64>;
