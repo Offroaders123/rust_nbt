7fbef86319dc47e9e3093881dc0d780626fe1c75
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-12-21 23:16:09 -0800

    Reset to Previous
    Going back to a clean slate to start implementing my new findings!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::fs;
 mod tag;
-// use crate::tag::{ Tag, IntTag };
-use crate::tag::*;
 
 fn main() {
     let file: &str = "./test/hello_world.nbt";
@@ -9,13 +7,4 @@ fn main() {
 
     let data: Vec<u8> = fs::read(file).unwrap();
     println!("{:?}", &data);
-
-    let haha: Tag = Tag::Byte(25);
-    println!("{:?}", haha);
-
-    let hahaha: IntTag = 25;
-    println!("{:?}", hahaha);
-
-    let hahahaha: IntTag = Tag::Int(25);
-    println!("{:?}", hahahaha);
 }
