97b07c88c122cedb9495fa996f8549c732bf3cae
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-12-23 12:55:26 -0800

    Compression Extensions
    Extending these functions to more than one format, got more help from GPT :)
    
    Had to mangle the output a bit though, it wasn't working completely, this seems to be correct now though.

--- src/compression.rs
+++ src/compression.rs
@@ -2,18 +2,32 @@ use flate2::write::{ZlibDecoder, ZlibEncoder};
 use flate2::Compression;
 use std::io::{Result, Write};
 
+// Enum for compression formats
+pub enum CompressionFormat {
+    Zlib,
+    // Add other formats like Gzip if needed
+}
+
 // Compress data
-pub fn compress(data: &[u8]) -> Result<Vec<u8>> {
-    let mut encoder: ZlibEncoder<Vec<u8>> = ZlibEncoder::new(Vec::new(), Compression::default());
-    encoder.write_all(data)?;
-    encoder.finish()
+pub fn compress(data: &[u8], format: CompressionFormat) -> Result<Vec<u8>> {
+    match format {
+        CompressionFormat::Zlib => {
+            let mut encoder: ZlibEncoder<Vec<u8>> = ZlibEncoder::new(Vec::new(), Compression::default());
+            encoder.write_all(data)?;
+            encoder.finish()
+        }
+    }
 }
 
 // Decompress data
-pub fn decompress(data: &[u8]) -> Result<Vec<u8>> {
-    let mut decoder: ZlibDecoder<Vec<u8>> = ZlibDecoder::new(Vec::new());
-    decoder.write_all(data)?;
-    decoder.finish()
+pub fn decompress(data: &[u8], format: CompressionFormat) -> Result<Vec<u8>> {
+    match format {
+        CompressionFormat::Zlib => {
+            let mut decoder: ZlibDecoder<Vec<u8>> = ZlibDecoder::new(Vec::new());
+            decoder.write_all(data)?;
+            decoder.finish()
+        }
+    }
 }
 
 fn main() -> Result<()> {
@@ -21,11 +35,11 @@ fn main() -> Result<()> {
     println!("Original: {:?}", input);
 
     // Compress
-    let compressed = compress(input)?;
+    let compressed = compress(input, CompressionFormat::Zlib)?;
     println!("Compressed: {:?}", compressed);
 
     // Decompress
-    let decompressed = decompress(&compressed)?;
+    let decompressed = decompress(&compressed, CompressionFormat::Zlib)?;
     println!("Decompressed: {:?}", decompressed);
 
     Ok(())
