f725bc44085e6a0fe67b6f736a6b4617070c4781
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-12-23 00:25:10 -0800

    Library Modules!
    Aaah, that makes more sense now. So it was declaring the module, not exporting things from it.
    
    Empath: Gulag, Middle Aged Man
    Gulag has been in my head the last few days here and there, it was interesting because I wasn't sure I really liked that song when I got it around last Christmas.
    https://www.reddit.com/r/learnrust/comments/10wgz9t/what_goes_into_librs/
    https://www.reddit.com/r/rust/comments/kv8iev/what_is_the_idiomatic_way_of_embed_files_into/ (neat)
    https://stackoverflow.com/questions/71165771/add-element-to-immutable-vector-rust

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::fs;
+use rust_nbt::tag::{ListTag, Tag};
 
-fn main() {
+fn main() -> () {
     let file: &str = "./test/hello_world.nbt";
     println!("{}", file);
 
     let data: Vec<u8> = fs::read(file).unwrap();
     println!("{:?}", &data);
+
+    let mut list: ListTag<Tag> = Vec::new();
+    list.push(Tag::Byte(5));
+    println!("{:?}", list);
 }
