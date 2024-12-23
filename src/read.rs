aebe1ed7626f47e2438c07222a37848e7889ea99
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-12-23 10:31:23 -0800

    Explicit Annotations
    Been practicing this over in TS for a while now, and it has been helping me keep a better mental model and keep track of what things are doing under the hood. I really like types now! While these weren't necessary to define, they show that their intent is, as well as their usage. I think this is a powerful tool. It's not really about typing the smallest of niche parts of the code, but showing you underlying patterns and common themes. This usually helps me abstract things out too, when you start to see common data patterns you wouldn't otherwise.
    
    Plague Accommodations
    
    Also I think I can fix the match ambiguity for read_tag in that tag_id can be an enum instead of a `u8`. I'll try that soon.

--- src/read.rs
+++ src/read.rs
@@ -1,15 +1,16 @@
 use crate::tag::Tag;
+use crate::{ByteArrayTag, CompoundTag, ListTag};
 use std::collections::HashMap;
 use std::io::{Cursor, Error, ErrorKind, Read, Result};
 
 /// Reads an NBT file from a byte vector and returns its root compound tag.
 pub fn read(data: Vec<u8>) -> Result<Tag> {
-    let mut cursor = Cursor::new(data);
-    let root_tag_id = read_u8(&mut cursor)?;
-    let name_length = read_u16(&mut cursor)? as usize;
-    let mut name_buffer = vec![0; name_length];
+    let mut cursor: Cursor<Vec<u8>> = Cursor::new(data);
+    let root_tag_id: u8 = read_u8(&mut cursor)?;
+    let name_length: usize = read_u16(&mut cursor)? as usize;
+    let mut name_buffer: Vec<u8> = vec![0; name_length];
     cursor.read_exact(&mut name_buffer)?;
-    let _root_name = String::from_utf8(name_buffer).unwrap();
+    let _root_name: String = String::from_utf8(name_buffer).unwrap();
     read_tag(&mut cursor, root_tag_id)
 }
 
@@ -24,53 +25,53 @@ fn read_tag<R: Read>(reader: &mut R, tag_id: u8) -> Result<Tag> {
         5 => Ok(Tag::Float(read_f32(reader)?)),
         6 => Ok(Tag::Double(read_f64(reader)?)),
         7 => {
-            let length = read_i32(reader)? as usize;
-            let mut data = vec![0; length];
+            let length: usize = read_i32(reader)? as usize;
+            let mut data: ByteArrayTag = vec![0; length];
             reader.read_exact(&mut data)?;
             Ok(Tag::ByteArray(data))
         }
         8 => {
-            let length = read_u16(reader)? as usize;
-            let mut buffer = vec![0; length];
+            let length: usize = read_u16(reader)? as usize;
+            let mut buffer: Vec<u8> = vec![0; length];
             reader.read_exact(&mut buffer)?;
             Ok(Tag::String(String::from_utf8(buffer).unwrap()))
         }
         9 => {
-            let item_type = read_u8(reader)?;
-            let length = read_i32(reader)? as usize;
-            let mut list = Vec::with_capacity(length);
+            let item_type: u8 = read_u8(reader)?;
+            let length: usize = read_i32(reader)? as usize;
+            let mut list: ListTag<Tag> = Vec::with_capacity(length);
             for _ in 0..length {
                 list.push(read_tag(reader, item_type)?);
             }
             Ok(Tag::List(list))
         }
         10 => {
-            let mut compound = HashMap::new();
+            let mut compound: CompoundTag = HashMap::new();
             loop {
-                let next_tag_id = read_u8(reader)?;
+                let next_tag_id: u8 = read_u8(reader)?;
                 if next_tag_id == 0 {
                     break;
                 }
-                let name_length = read_u16(reader)? as usize;
-                let mut name_buffer = vec![0; name_length];
+                let name_length: usize = read_u16(reader)? as usize;
+                let mut name_buffer: Vec<u8> = vec![0; name_length];
                 reader.read_exact(&mut name_buffer)?;
-                let name = String::from_utf8(name_buffer).unwrap();
-                let tag = read_tag(reader, next_tag_id)?;
+                let name: String = String::from_utf8(name_buffer).unwrap();
+                let tag: Tag = read_tag(reader, next_tag_id)?;
                 compound.insert(name, tag);
             }
             Ok(Tag::Compound(compound))
         }
         11 => {
-            let length = read_i32(reader)? as usize;
-            let mut data = Vec::with_capacity(length);
+            let length: usize = read_i32(reader)? as usize;
+            let mut data: Vec<i32> = Vec::with_capacity(length);
             for _ in 0..length {
                 data.push(read_i32(reader)?);
             }
             Ok(Tag::IntArray(data))
         }
         12 => {
-            let length = read_i32(reader)? as usize;
-            let mut data = Vec::with_capacity(length);
+            let length: usize = read_i32(reader)? as usize;
+            let mut data: Vec<i64> = Vec::with_capacity(length);
             for _ in 0..length {
                 data.push(read_i64(reader)?);
             }
@@ -82,7 +83,7 @@ fn read_tag<R: Read>(reader: &mut R, tag_id: u8) -> Result<Tag> {
 
 /// Helper functions to read various data types from a reader.
 fn read_u8<R: Read>(reader: &mut R) -> Result<u8> {
-    let mut buffer = [0; 1];
+    let mut buffer: [u8; 1] = [0; 1];
     reader.read_exact(&mut buffer)?;
     Ok(buffer[0])
 }
@@ -92,7 +93,7 @@ fn read_i8<R: Read>(reader: &mut R) -> Result<i8> {
 }
 
 fn read_u16<R: Read>(reader: &mut R) -> Result<u16> {
-    let mut buffer = [0; 2];
+    let mut buffer: [u8; 2] = [0; 2];
     reader.read_exact(&mut buffer)?;
     Ok(u16::from_be_bytes(buffer))
 }
@@ -102,25 +103,25 @@ fn read_i16<R: Read>(reader: &mut R) -> Result<i16> {
 }
 
 fn read_i32<R: Read>(reader: &mut R) -> Result<i32> {
-    let mut buffer = [0; 4];
+    let mut buffer: [u8; 4] = [0; 4];
     reader.read_exact(&mut buffer)?;
     Ok(i32::from_be_bytes(buffer))
 }
 
 fn read_i64<R: Read>(reader: &mut R) -> Result<i64> {
-    let mut buffer = [0; 8];
+    let mut buffer: [u8; 8] = [0; 8];
     reader.read_exact(&mut buffer)?;
     Ok(i64::from_be_bytes(buffer))
 }
 
 fn read_f32<R: Read>(reader: &mut R) -> Result<f32> {
-    let mut buffer = [0; 4];
+    let mut buffer: [u8; 4] = [0; 4];
     reader.read_exact(&mut buffer)?;
     Ok(f32::from_be_bytes(buffer))
 }
 
 fn read_f64<R: Read>(reader: &mut R) -> Result<f64> {
-    let mut buffer = [0; 8];
+    let mut buffer: [u8; 8] = [0; 8];
     reader.read_exact(&mut buffer)?;
     Ok(f64::from_be_bytes(buffer))
 }
