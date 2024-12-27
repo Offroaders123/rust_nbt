8bbeece36e12e82ac12ea46832157860ea3d0b68
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-12-27 01:32:03 -0800

    Demo Cleanup
    Cleaning up the test a bit, now that I know how things work a bit more. Loving this language now!!!
    
    Also coming up with a concept where one could potentially use WASM just for this stage of things, and it would return a nice JS object you could work with in another context, to which this would do all of the heavy lifting for you. Loving the sound of this.
    
    Also, just had another idea click. Pattern matching and traits feels like the best way to make NBT conversions work. It only just about makes absolute perfect sense. I might just be a little too ambitious though, we'll see! I think I get how powerful this is now.
    
    https://stackoverflow.com/questions/67859926/how-to-run-cargo-fmt-on-save-in-vscode
    https://code.visualstudio.com/docs/languages/rust#_formatting
    https://stackoverflow.com/questions/55755552/what-is-the-rust-equivalent-to-a-try-catch-statement
    https://stackoverflow.com/questions/39785597/how-do-i-get-a-slice-of-a-vect-in-rust (This syntax is absolute fire 🔥)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_nbt::{decompress, read, write, ListTag, Tag};
+use rust_nbt::{decompress, read, write, Tag};
 use std::fs;
 use std::io::Result;
 
@@ -8,18 +8,17 @@ fn main() -> Result<()> {
 
     let nbt_bytes: Vec<u8> =
         decompress(&fs::read(file).unwrap(), rust_nbt::CompressionFormat::Gzip)?;
-    println!("{:?}", &nbt_bytes);
-
-    let list: ListTag<Tag> = vec![Tag::Byte(5)];
-    println!("{:?}", list);
+    println!("{:?}", &nbt_bytes[0..10]);
 
     // Example usage: Pass an NBT file's binary contents as a Vec<u8>
     let nbt_data: Tag = read(&nbt_bytes)?;
     println!("{:#?}", nbt_data);
 
     let recompile: Vec<u8> = write(&nbt_data, "Level")?;
-    println!("{:?}", &recompile);
+    println!("{:?}", &recompile[0..10]);
 
     assert_eq!(&nbt_bytes, &recompile);
+    println!("Successful r/w!");
+
     Ok(())
 }
