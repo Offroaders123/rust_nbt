6fb7e3fce201342f2fe891aca110d422bcde2da0
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-01-02 00:20:20 -0800

    NBTify Function Symmetricality
    Moved the implementation names over to match that of NBTify, since this is essentially intended now to be a native port of NBTify to Rust. I didn't initially plan for this to be the case, but conceptually I want to try it out because I think it will help me learn the differences between Rust and TS, as that is my main set of knowledge for programming up to this point. While you can't always prevent it from occurring, I want to not have many misconceptions on how I think things work.

--- src/read.rs
+++ src/read.rs
@@ -6,8 +6,8 @@ use std::io::{Cursor, Error, ErrorKind, Read, Result};
 /// Reads an NBT file from a byte vector and returns its root compound tag.
 pub fn read(data: &[u8]) -> Result<Tag> {
     let mut cursor: Cursor<&[u8]> = Cursor::new(&data);
-    let root_tag_id: u8 = read_u8(&mut cursor)?;
-    let name_length: usize = read_u16(&mut cursor)? as usize;
+    let root_tag_id: u8 = read_unsigned_byte(&mut cursor)?;
+    let name_length: usize = read_unsigned_short(&mut cursor)? as usize;
     let mut name_buffer: Vec<u8> = vec![0; name_length];
     cursor.read_exact(&mut name_buffer)?;
     let root_name: String = String::from_utf8(name_buffer).unwrap();
@@ -19,12 +19,12 @@ pub fn read(data: &[u8]) -> Result<Tag> {
 fn read_tag<R: Read>(reader: &mut R, tag_id: u8) -> Result<Tag> {
     match tag_id {
         0 => Ok(Tag::End),
-        1 => Ok(Tag::Byte(read_i8(reader)?)),
-        2 => Ok(Tag::Short(read_i16(reader)?)),
-        3 => Ok(Tag::Int(read_i32(reader)?)),
-        4 => Ok(Tag::Long(read_i64(reader)?)),
-        5 => Ok(Tag::Float(read_f32(reader)?)),
-        6 => Ok(Tag::Double(read_f64(reader)?)),
+        1 => Ok(Tag::Byte(read_byte(reader)?)),
+        2 => Ok(Tag::Short(read_short(reader)?)),
+        3 => Ok(Tag::Int(read_int(reader)?)),
+        4 => Ok(Tag::Long(read_long(reader)?)),
+        5 => Ok(Tag::Float(read_float(reader)?)),
+        6 => Ok(Tag::Double(read_double(reader)?)),
         7 => Ok(Tag::ByteArray(read_byte_array(reader)?)),
         8 => Ok(Tag::String(read_string(reader)?)),
         9 => Ok(Tag::List(read_list(reader)?)),
@@ -36,69 +36,69 @@ fn read_tag<R: Read>(reader: &mut R, tag_id: u8) -> Result<Tag> {
 }
 
 /// Helper functions to read various data types from a reader.
-fn read_u8<R: Read>(reader: &mut R) -> Result<u8> {
+fn read_unsigned_byte<R: Read>(reader: &mut R) -> Result<u8> {
     let mut buffer: [u8; 1] = [0; 1];
     reader.read_exact(&mut buffer)?;
     Ok(buffer[0])
 }
 
-fn read_i8<R: Read>(reader: &mut R) -> Result<i8> {
-    Ok(read_u8(reader)? as i8)
+fn read_byte<R: Read>(reader: &mut R) -> Result<i8> {
+    Ok(read_unsigned_byte(reader)? as i8)
 }
 
-fn read_u16<R: Read>(reader: &mut R) -> Result<u16> {
+fn read_unsigned_short<R: Read>(reader: &mut R) -> Result<u16> {
     let mut buffer: [u8; 2] = [0; 2];
     reader.read_exact(&mut buffer)?;
     Ok(u16::from_be_bytes(buffer))
 }
 
-fn read_i16<R: Read>(reader: &mut R) -> Result<i16> {
-    Ok(read_u16(reader)? as i16)
+fn read_short<R: Read>(reader: &mut R) -> Result<i16> {
+    Ok(read_unsigned_short(reader)? as i16)
 }
 
-fn read_i32<R: Read>(reader: &mut R) -> Result<i32> {
+fn read_int<R: Read>(reader: &mut R) -> Result<i32> {
     let mut buffer: [u8; 4] = [0; 4];
     reader.read_exact(&mut buffer)?;
     Ok(i32::from_be_bytes(buffer))
 }
 
-fn read_i64<R: Read>(reader: &mut R) -> Result<i64> {
+fn read_long<R: Read>(reader: &mut R) -> Result<i64> {
     let mut buffer: [u8; 8] = [0; 8];
     reader.read_exact(&mut buffer)?;
     Ok(i64::from_be_bytes(buffer))
 }
 
-fn read_f32<R: Read>(reader: &mut R) -> Result<f32> {
+fn read_float<R: Read>(reader: &mut R) -> Result<f32> {
     let mut buffer: [u8; 4] = [0; 4];
     reader.read_exact(&mut buffer)?;
     Ok(f32::from_be_bytes(buffer))
 }
 
-fn read_f64<R: Read>(reader: &mut R) -> Result<f64> {
+fn read_double<R: Read>(reader: &mut R) -> Result<f64> {
     let mut buffer: [u8; 8] = [0; 8];
     reader.read_exact(&mut buffer)?;
     Ok(f64::from_be_bytes(buffer))
 }
 
 fn read_byte_array<R: Read>(reader: &mut R) -> Result<ByteArrayTag> {
-    let length: usize = read_i32(reader)? as usize;
+    let length: usize = read_int(reader)? as usize;
     let mut data: ByteArrayTag = Vec::with_capacity(length);
     for _ in 0..length {
-        data.push(read_i8(reader)?);
+        data.push(read_byte(reader)?);
     }
     Ok(data)
 }
 
 fn read_string<R: Read>(reader: &mut R) -> Result<StringTag> {
-    let length: usize = read_u16(reader)? as usize;
+    let length: usize = read_unsigned_short(reader)? as usize;
     let mut buffer: Vec<u8> = vec![0; length];
     reader.read_exact(&mut buffer)?;
     Ok(String::from_utf8(buffer).unwrap())
 }
 
 fn read_list<R: Read>(reader: &mut R) -> Result<ListTag<Tag>> {
-    let item_type: u8 = read_u8(reader)?;
-    let length: usize = read_i32(reader)? as usize;
+    let item_type: u8 = read_unsigned_byte(reader)?;
+    let length: usize = read_int(reader)? as usize;
     let mut list: ListTag<Tag> = Vec::with_capacity(length);
     for _ in 0..length {
         list.push(read_tag(reader, item_type)?);
@@ -109,11 +109,11 @@ fn read_list<R: Read>(reader: &mut R) -> Result<ListTag<Tag>> {
 fn read_compound<R: Read>(reader: &mut R) -> Result<CompoundTag> {
     let mut compound: CompoundTag = IndexMap::new();
     loop {
-        let next_tag_id: u8 = read_u8(reader)?;
+        let next_tag_id: u8 = read_unsigned_byte(reader)?;
         if next_tag_id == 0 {
             break;
         }
-        let name_length: usize = read_u16(reader)? as usize;
+        let name_length: usize = read_unsigned_short(reader)? as usize;
         let mut name_buffer: Vec<u8> = vec![0; name_length];
         reader.read_exact(&mut name_buffer)?;
         let name: String = String::from_utf8(name_buffer).unwrap();
@@ -124,19 +124,19 @@ fn read_compound<R: Read>(reader: &mut R) -> Result<CompoundTag> {
 }
 
 fn read_int_array<R: Read>(reader: &mut R) -> Result<IntArrayTag> {
-    let length: usize = read_i32(reader)? as usize;
+    let length: usize = read_int(reader)? as usize;
     let mut data: Vec<i32> = Vec::with_capacity(length);
     for _ in 0..length {
-        data.push(read_i32(reader)?);
+        data.push(read_int(reader)?);
     }
     Ok(data)
 }
 
 fn read_long_array<R: Read>(reader: &mut R) -> Result<LongArrayTag> {
-    let length: usize = read_i32(reader)? as usize;
+    let length: usize = read_int(reader)? as usize;
     let mut data: Vec<i64> = Vec::with_capacity(length);
     for _ in 0..length {
-        data.push(read_i64(reader)?);
+        data.push(read_long(reader)?);
     }
     Ok(data)
 }
--- src/write.rs
+++ src/write.rs
@@ -4,8 +4,8 @@ use std::io::{Cursor, Result, Write};
 /// Writes an NBT file to a byte vector, starting with the root compound tag.
 pub fn write(tag: &Tag, root_name: &str) -> Result<Vec<u8>> {
     let mut cursor: Cursor<Vec<u8>> = Cursor::new(Vec::new());
-    write_u8(&mut cursor, tag.id())?;
-    write_u16(&mut cursor, root_name.len() as u16)?;
+    write_unsigned_byte(&mut cursor, tag.id())?;
+    write_unsigned_short(&mut cursor, root_name.len() as u16)?;
     cursor.write_all(root_name.as_bytes())?;
     write_tag(&mut cursor, tag)?;
     Ok(cursor.into_inner())
@@ -15,12 +15,12 @@ pub fn write(tag: &Tag, root_name: &str) -> Result<Vec<u8>> {
 fn write_tag<W: Write>(writer: &mut W, tag: &Tag) -> Result<()> {
     match tag {
         Tag::End => Ok(()), // End tag has no payload.
-        Tag::Byte(value) => write_i8(writer, *value),
-        Tag::Short(value) => write_i16(writer, *value),
-        Tag::Int(value) => write_i32(writer, *value),
-        Tag::Long(value) => write_i64(writer, *value),
-        Tag::Float(value) => write_f32(writer, *value),
-        Tag::Double(value) => write_f64(writer, *value),
+        Tag::Byte(value) => write_byte(writer, *value),
+        Tag::Short(value) => write_short(writer, *value),
+        Tag::Int(value) => write_int(writer, *value),
+        Tag::Long(value) => write_long(writer, *value),
+        Tag::Float(value) => write_float(writer, *value),
+        Tag::Double(value) => write_double(writer, *value),
         Tag::ByteArray(data) => write_byte_array(writer, data),
         Tag::String(value) => write_string(writer, value),
         Tag::List(list) => write_list(writer, list),
@@ -31,87 +31,87 @@ fn write_tag<W: Write>(writer: &mut W, tag: &Tag) -> Result<()> {
 }
 
 /// Helper functions to write various data types to a writer.
-fn write_u8<W: Write>(writer: &mut W, value: u8) -> Result<()> {
+fn write_unsigned_byte<W: Write>(writer: &mut W, value: u8) -> Result<()> {
     writer.write_all(&[value])
 }
 
-fn write_i8<W: Write>(writer: &mut W, value: i8) -> Result<()> {
-    write_u8(writer, value as u8)
+fn write_byte<W: Write>(writer: &mut W, value: i8) -> Result<()> {
+    write_unsigned_byte(writer, value as u8)
 }
 
-fn write_u16<W: Write>(writer: &mut W, value: u16) -> Result<()> {
+fn write_unsigned_short<W: Write>(writer: &mut W, value: u16) -> Result<()> {
     writer.write_all(&value.to_be_bytes())
 }
 
-fn write_i16<W: Write>(writer: &mut W, value: i16) -> Result<()> {
-    write_u16(writer, value as u16)
+fn write_short<W: Write>(writer: &mut W, value: i16) -> Result<()> {
+    write_unsigned_short(writer, value as u16)
 }
 
-fn write_i32<W: Write>(writer: &mut W, value: i32) -> Result<()> {
+fn write_int<W: Write>(writer: &mut W, value: i32) -> Result<()> {
     writer.write_all(&value.to_be_bytes())
 }
 
-fn write_i64<W: Write>(writer: &mut W, value: i64) -> Result<()> {
+fn write_long<W: Write>(writer: &mut W, value: i64) -> Result<()> {
     writer.write_all(&value.to_be_bytes())
 }
 
-fn write_f32<W: Write>(writer: &mut W, value: f32) -> Result<()> {
+fn write_float<W: Write>(writer: &mut W, value: f32) -> Result<()> {
     writer.write_all(&value.to_be_bytes())
 }
 
-fn write_f64<W: Write>(writer: &mut W, value: f64) -> Result<()> {
+fn write_double<W: Write>(writer: &mut W, value: f64) -> Result<()> {
     writer.write_all(&value.to_be_bytes())
 }
 
 fn write_byte_array<W: Write>(writer: &mut W, data: &ByteArrayTag) -> Result<()> {
-    write_i32(writer, data.len() as i32)?;
+    write_int(writer, data.len() as i32)?;
     for value in data {
-        write_i8(writer, *value)?;
+        write_byte(writer, *value)?;
     }
     Ok(())
 }
 
 fn write_string<W: Write>(writer: &mut W, value: &StringTag) -> Result<()> {
-    write_u16(writer, value.len() as u16)?;
+    write_unsigned_short(writer, value.len() as u16)?;
     writer.write_all(value.as_bytes())
 }
 
 fn write_list<W: Write>(writer: &mut W, list: &ListTag<Tag>) -> Result<()> {
     if let Some(first_item) = list.first() {
-        write_u8(writer, first_item.id())?;
-        write_i32(writer, list.len() as i32)?;
+        write_unsigned_byte(writer, first_item.id())?;
+        write_int(writer, list.len() as i32)?;
         for item in list {
             write_tag(writer, item)?;
         }
     } else {
-        write_u8(writer, 0)?; // Empty list type.
-        write_i32(writer, 0)?; // Empty list length.
+        write_unsigned_byte(writer, 0)?; // Empty list type.
+        write_int(writer, 0)?; // Empty list length.
     }
     Ok(())
 }
 
 fn write_compound<W: Write>(writer: &mut W, compound: &CompoundTag) -> Result<()> {
     for (key, value) in compound {
-        write_u8(writer, value.id())?;
-        write_u16(writer, key.len() as u16)?;
+        write_unsigned_byte(writer, value.id())?;
+        write_unsigned_short(writer, key.len() as u16)?;
         writer.write_all(key.as_bytes())?;
         write_tag(writer, value)?;
     }
-    write_u8(writer, 0) // End tag for compound.
+    write_unsigned_byte(writer, 0) // End tag for compound.
 }
 
 fn write_int_array<W: Write>(writer: &mut W, data: &IntArrayTag) -> Result<()> {
-    write_i32(writer, data.len() as i32)?;
+    write_int(writer, data.len() as i32)?;
     for value in data {
-        write_i32(writer, *value)?;
+        write_int(writer, *value)?;
     }
     Ok(())
 }
 
 fn write_long_array<W: Write>(writer: &mut W, data: &LongArrayTag) -> Result<()> {
-    write_i32(writer, data.len() as i32)?;
+    write_int(writer, data.len() as i32)?;
     for value in data {
-        write_i64(writer, *value)?;
+        write_long(writer, *value)?;
     }
     Ok(())
 }
