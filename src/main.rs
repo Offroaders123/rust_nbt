9db1bb238a8e4819791f9812d9d4ae82ff09cfbc
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-08-14 22:42:17 -0700

    LittleEndian Generic Properly Done!
    Heck yeah! It's working.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use byteorder::BigEndian;
+use byteorder::LittleEndian;
 use rust_nbt::{decompress, read_root, write_root, Tag};
 use std::fs;
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let file: &str = "./test/bigtest.nbt";
+    let file: &str = "./test/bigtest_little.nbt";
     println!("{}", file);
 
     let nbt_bytes: Vec<u8> =
@@ -12,10 +12,10 @@ fn main() -> Result<()> {
     println!("{:?}", &nbt_bytes[0..10]);
 
     // Example usage: Pass an NBT file's binary contents as a Vec<u8>
-    let nbt_data: Tag = read_root::<BigEndian>(&nbt_bytes)?;
+    let nbt_data: Tag = read_root::<LittleEndian>(&nbt_bytes)?;
     println!("{:?}", nbt_data);
 
-    let recompile: Vec<u8> = write_root::<BigEndian>(&nbt_data, "Level")?;
+    let recompile: Vec<u8> = write_root::<LittleEndian>(&nbt_data, "Level")?;
     println!("{:?}", &recompile[0..10]);
 
     assert_eq!(&nbt_bytes, &recompile);
