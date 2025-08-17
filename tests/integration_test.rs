bb1dc5acc61cf7d0f9394d7f4b34b48a09db8ec0
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-08-17 10:49:05 -0700

    Big Endian Test
    https://doc.rust-lang.org/book/ch11-03-test-organization.html
    https://crates.io/crates/fixtures
    https://github.com/bcheidemann/fixtures-rs
    https://github.com/bcheidemann/entype

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,27 @@
-use byteorder::LittleEndian;
+use byteorder::{BigEndian, LittleEndian};
 use rust_nbt::{decompress, read_root, write_root, BedrockHeader, CompressionFormat, Tag};
 use std::{fs::read, io::Result};
+
+#[test]
+fn symmetrical_nbt_be() -> Result<()> {
+    let file: &str = "./tests/bigtest.nbt";
+    println!("{}", file);
+
+    let nbt_bytes: Vec<u8> = decompress(&read(file).unwrap(), rust_nbt::CompressionFormat::Gzip)?;
+    println!("{:?}", &nbt_bytes[0..10]);
+
+    // Example usage: Pass an NBT file's binary contents as a Vec<u8>
+    let nbt_data: Tag = read_root::<BigEndian>(&nbt_bytes, BedrockHeader::Without)?;
+    println!("{:?}", nbt_data);
+
+    let recompile: Vec<u8> = write_root::<BigEndian>(&nbt_data, "Level", BedrockHeader::Without)?;
+    println!("{:?}", &recompile[0..10]);
+
+    assert_eq!(&nbt_bytes, &recompile);
+    println!("Successful r/w!");
+
+    Ok(())
+}
 
 #[test]
 fn symmetrical_nbt_le() -> Result<()> {
