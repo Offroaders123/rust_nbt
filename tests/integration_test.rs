9c2eccb877ff43e7f8abc341ec3161859fc050c4
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-08-17 10:55:47 -0700

    Dedicated Little Endian Test
    Okay, now this is set up properly.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -24,6 +24,28 @@ fn symmetrical_nbt_be() -> Result<()> {
 }
 
 #[test]
+fn symmetrical_nbt_le() -> Result<()> {
+    let file: &str = "./tests/bigtest_little.nbt";
+    println!("{}", file);
+
+    let nbt_bytes: Vec<u8> = decompress(&read(file).unwrap(), CompressionFormat::Gzip)?;
+    println!("{:?}", &nbt_bytes[0..10]);
+
+    // Example usage: Pass an NBT file's binary contents as a Vec<u8>
+    let nbt_data: Tag = read_root::<LittleEndian>(&nbt_bytes, BedrockHeader::Without)?;
+    println!("{:?}", nbt_data);
+
+    let recompile: Vec<u8> =
+        write_root::<LittleEndian>(&nbt_data, "Level", BedrockHeader::Without)?;
+    println!("{:?}", &recompile[0..10]);
+
+    assert_eq!(&nbt_bytes, &recompile);
+    println!("Successful r/w!");
+
+    Ok(())
+}
+
+#[test]
 fn symmetrical_nbt_le_bedrock_header() -> Result<()> {
     let file: &str = "./tests/level.dat";
     println!("{}", file);
